eda377208a4bd50b3428fafb777341a78ec8a0a5
ai1solution <aionesolution07@gmail.com>
2025-04-28 19:17:16 +0530

    hosting

--- utils.py
+++ utils.py
@@ -7,4 +7,4 @@ load_dotenv()
 # Fetch secret values from environment
 SERPAPI_API_KEY = os.getenv('SERPAPI_API_KEY')
 
-DEFAULT_IMAGE_URL = "https://ibb.co/81mTf6g"
+DEFAULT_IMAGE_URL = "https://i.ibb.co/7sS8Z2t/Being.png"
